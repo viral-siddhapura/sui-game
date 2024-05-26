544f75f36b6791caf30425d84c6f7e84b5a4c2ae
Jordan <>
2024-05-26 19:17:03 +0530

    Google Login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,7 +1,23 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { GoogleLogin } from '@react-oauth/google';
 
-export default Login
+const Login = () => {
+  const responseMessage = (response) => {
+    console.log('Google login response: ', response);
+  };
+  const errorMessage = (error) => {
+    console.log('Google login error: ', error);
+  };
+  return (
+    <div className='w-full h-screen bg-gray-950'>
+      <div className='text-center text-white'>
+        <h1>Login to SuiFrenia</h1>
+        <h2>Using ZkLogin</h2>
+        <div className='w-56 text-center mx-auto py-8'>
+          <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
