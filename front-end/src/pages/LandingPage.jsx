162478e54ad4603d526934c0d37fcc35efe5e310
Jordan <>
2024-06-01 21:21:02 +0530

    battleground-page

--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -6,7 +6,7 @@ import land6 from '/land6.png';
 import { Link } from 'react-router-dom';
 const LandingPage = () => {
   return (
-    <div className='flex flex-col justify w-screen h-screen content center bg-violet-950'>
+    <div className="flex flex-col justify w-screen h-screen content center bg-violet-950 overflow-x-hidden">
       <div>
         <img src={land2} alt='' />
       </div>
@@ -22,9 +22,9 @@ const LandingPage = () => {
       <div>
         <img src={land6} alt='' />
       </div>
-      <div className='absolute top-4 right-4 '>
-        <Link to={'/login'}>
-          <button className='bg-gradient-to-r from-[#f0d040] to-[#cd8f09] w-[200px] h-[50px] rounded-lg font-bold market-cards'>
+      <div className="absolute top-4 right-8 ">
+        <Link to={"/login"}>
+          <button className="bg-gradient-to-r from-[#f0d040] to-[#cd8f09] w-[200px] h-[50px] rounded-lg font-bold market-cards">
             Login
           </button>
         </Link>
