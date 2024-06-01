90cef5fc532c8853f3135c520b1cb1d93e6401d3
Jordan <>
2024-06-01 21:30:48 +0530

    Landing Page Styling

--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import land2 from "../../public/landin2.png";
-import land3 from "../../public/land3.png";
-import land4 from "../../public/land4.png";
-import land5 from "../../public/land5.png";
-import land6 from "../../public/land6.png";
+import land1 from "../../public/1.png";
+import land2 from "../../public/2.png";
+import land3 from "../../public/3.png";
+import land4 from "../../public/4.png";
+import land5 from "../../public/5.png";
+import land6 from "../../public/6.png";
+import land7 from "../../public/7.png";
+
 import { Link } from "react-router-dom";
 const LandingPage = () => {
   return (
-    <div className="flex flex-col justify w-screen h-screen content center bg-violet-950">
+    <div className="flex flex-col justify w-screen overflow-x-hidden no-scrollbar h-screen content center bg-violet-950">
+      <div>
+        <img src={land1} alt="" />
+      </div>
       <div>
         <img src={land2} alt="" />
       </div>
@@ -23,10 +29,13 @@ const LandingPage = () => {
       <div>
         <img src={land6} alt="" />
       </div>
-      <div className="absolute top-4 right-4 ">
+      <div>
+        <img src={land7} alt="" />
+      </div>
+      <div className="fixed top-4 right-[20px]">
         <Link to={"/login"}>
           <button className="bg-gradient-to-r from-[#f0d040] to-[#cd8f09] w-[200px] h-[50px] rounded-lg font-bold market-cards">
-            Login
+            PLAY NOW
           </button>
         </Link>
       </div>
